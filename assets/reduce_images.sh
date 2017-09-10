d61ff7066c8f0fdfd2131ad7c3dd56f9fe6a759e
Jamie Cho <jamie@ginkgobioworks.com>
2017-09-10 16:51:41 -0400

    Add more game images

--- assets/reduce_images.sh
+++ assets/reduce_images.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 ../scripts/make-coco-palette.py --alpha palette.gif
+convert -remap palette.gif -colors 18 +dither 00-mainmenu.png ../game/images/00-mainmenu.png
 convert -remap palette.gif -colors 18 +dither 01-moon.gif ../game/tiles/01-moon.gif
