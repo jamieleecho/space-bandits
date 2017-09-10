23e893945fc462ed41af053ce6644759c98e9cf8
Jamie Cho <jamie@ginkgobioworks.com>
2017-09-10 17:52:09 -0400

    Clean up, begin missile logic

---
+++ game/objects/05-missile.c
@@ -0,0 +1,38 @@
+#pragma org 0x0
+#include "05-missile.h"
+
+
+byte didNotInit = TRUE;
+byte initVal = 0;
+
+
+void ObjectInit(DynospriteCOB *cob, DynospriteODT *odt, byte *initData) {
+  if (didNotInit) {
+    didNotInit = FALSE;
+  }
+}
+
+
+void ObjectReactivate(DynospriteCOB *cob, DynospriteODT *odt) {
+}
+
+
+void ObjectUpdate(DynospriteCOB *cob, DynospriteODT *odt) {
+  ShipObjectState *statePtr = (ShipObjectState *)(cob->statePtr);
+
+  if (cob->globalY < 20) {
+    cob->globalY = 140;
+  } else {
+    byte delta = ((DynospriteDirectPageGlobalsPtr->Obj_MotionFactor + 2)) << 1;
+    cob->globalY -= delta;
+  }
+}
+
+
+/** Ignored, only used to guarantee functions will not get optimized away */
+int main() {
+  ObjectInit((DynospriteCOB *)0x0, (DynospriteODT *)0x0, (byte *)0x0);
+  ObjectReactivate((DynospriteCOB *)0x0, (DynospriteODT *)0x0);
+  ObjectUpdate((DynospriteCOB *)0x0, (DynospriteODT *)0x0);
+  return 0;
+}
