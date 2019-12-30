d9b56383b9ea7f69f3075b378b17b1a57128073c
Jamie Cho <jamie@ginkgobioworks.com>
2019-12-30 08:03:09 -0500

    ???

---
+++ Space Bandits/dynosprite/src/DSCoCoKeyboardJoystick.h
@@ -0,0 +1,36 @@
+//
+//  DSCoCoKeyboardJoystick.h
+//  Space Bandits
+//
+//  Created by Jamie Cho on 8/10/19.
+//  Copyright © 2019 Jamie Cho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DSCoCoJoystickProtocol.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DSCoCoKeyboardJoystick : NSObject<DSCoCoJoystickProtocol> {
+    unsigned char _xAxisPosition;
+    unsigned char _yAxisPosition;
+    BOOL _button0Pressed;
+    BOOL _button1Pressed;
+    
+    BOOL _leftKeyIsPressed;
+    BOOL _rightKeyIsPressed;
+    BOOL _upKeyIsPressed;
+    BOOL _downKeyIsPressed;
+}
+
+- (instancetype)init;
+
+/** Must be called to handle keyboard events */
+- (void)handleKeyDown:(NSEvent *)event;
+
+/** Must be called to handle keyboard events */
+- (void)handleKeyUp:(NSEvent *)event;
+
+@end
+
+NS_ASSUME_NONNULL_END
